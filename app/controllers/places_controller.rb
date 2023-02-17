d584f3ad9140d775a00313aa7d1f7bcb190a212b
rezakumara2023 <rezakumara2023@u.northwestern.edu>
2023-02-17 23:54:51 +0000

    create new action

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,2 +1,16 @@
 class PlacesController < ApplicationController
+    def index
+    # get any data in
+
+    # handle business logic
+    end
+
+    def new
+    @place = Place.new
+    end
+    
+    def create
+    end
+
+
 end
