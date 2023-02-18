7feab2d2d69dc5b5848a9b2e76b1013a3065b47d
rezakumara2023 <rezakumara2023@u.northwestern.edu>
2023-02-18 01:46:23 +0000

    posts_error

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,20 @@
 class PostsController < ApplicationController
 end
 
- def index
-    # get any data in
-    @posts = Post.all
-    # handle business logic
-end
 
-def new
-    @post = Post.new
-end
+    def new
+        @post = Post.new
+        @place = Place.find_by({ "id" => params["id"] })
+        @post["place_id"] = @place["id"]
+    end
+
+    def create
+        @post = Post.new
+        @post["title"] = params["post"]["title"]
+        @post["description"] = params["post"]["description"]
+        @post["posted_on"] = params["post"]["posted_on"]
+        @post["place_id"] = params["post"]["place_id"]
+        @post.save
+        redirect_to "/places/#{@post["place_id"]}"
+    end
+
